707fab80eeb050425c8b2faa9dcc611cd258460f
Sebastian Banszky <sebastian.paul@banszky.men>
2025-09-01 09:00:40 +0300

    Fix missing useEffect import

--- src/components/IpMatrix.tsx
+++ src/components/IpMatrix.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useMatrixEffect } from '../hooks/useMatrixEffect';
 
 export const IpMatrix = () => {
